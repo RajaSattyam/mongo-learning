401988a6e45c9b3d19987ca57d250a1f298547fe
Ashutosh3436 <ashu.python@gmail.com>
2018-04-06 02:09:09 +0530

    env var

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env ****',env);
+
+if(env === 'development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';    
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');  
@@ -9,8 +20,9 @@ var {User} = require('./models/user');
 var {authenticate} = require('./middleware/authenticate')
 
 const app = express();
+const port = process.env.PORT;
 app.use(bodyParser.json());
-const port = process.env.PORT || 3000;
+
 
 
 app.post('/todos',(req,res) => {
